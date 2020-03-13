7853cb923da5b625b48c6aeb8af52ff10594dd82
Kelli Scheuble <kellischeuble@gmail.com>
2020-03-12 19:16:48 -0700

    adds test

--- my_lambdata/test.py
+++ my_lambdata/test.py
@@ -1,1 +1,10 @@
-import my_lambdata.module as lambdata
+from my_lambdata.module import Wrangle
+import unittest
+import pandas as pd
+
+class TestWrangle(unittest.TestCase):
+
+    def test_separate_dates(self):
+        df = pandas.DataFrame({"date": ["3-22-1939", "11-23-1953", "5-30-2001"]})
+        df = separate_dates(df, "date")
+        self.assertTrue('month' in df.columns.tolist())
